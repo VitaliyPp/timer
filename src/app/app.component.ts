224aeb3989a8f9a5fcf56ce7c0e74868a4aead8a
vitalii.pavlov <vitalii.pavlov@lasoft.org>
2022-07-04 03:12:44 +0300

    created new rep
    added some functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'timer';
+  title: any = 'timer';
+
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+  isRunning: boolean = false;
+  startTimer: any;
+
+  sub: Subscription;
+  intervalStream$ = interval(1000);
+
+  start(): void {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.sub = this.intervalStream$.subscribe(() => {
+
+        this.sec++;
+        this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+      })
+    } else {
+      this.stop();
+    }
+  }
+
+  stop(): void {
+    this.sub.unsubscribe();
+    this.isRunning = false;
+    this.sec = '0' + 0;
+    this.min = '0' + 0;
+  }
+
+  pause(): void {
+    this.sub.unsubscribe();
+    console.log(this.sub);
+    this.isRunning = !this.isRunning;
+  }
+
+  reset(): void {
+    this.sec = '0' + 0;
+    this.min = '0' + 0;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DebounceClickDirective } from './debounce-click.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DebounceClickDirective
   ],
   imports: [
     BrowserModule,
